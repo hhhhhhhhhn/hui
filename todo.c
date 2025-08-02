cc60f733a37bc718c27280ed0493f6cae1bf62c5
hhhhhhhhhn <joseugarte2262005@gmail.com>
2025-08-02 02:15:12 -0400

    Add Enter support for todo example

--- todo.c
+++ todo.c
@@ -38,10 +38,10 @@ i32 main(void) {
 				hui_scroll_start(&scroll);
 					hui_stack_start(0);
 						hui_stack_start(0);
-							hui_text_input(&text, &cursor, text_style);
+							u64 key = hui_text_input(&text, &cursor, text_style);
 							hui_leftright_start(0);
 								hui_nothing();
-								if (hui_button(1312, STR("Add"), text_style) && text.len > 0) {
+								if ((hui_button(1312, STR("Add"), text_style) || key == KEY_ENTER) && text.len > 0) {
 									todos[todos_len] = strb_from_str(str_from_strb(&text));
 									todos_len++;
 									text.len = 0;
